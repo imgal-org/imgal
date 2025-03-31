fb08d88005d64e24d14f8b794fcdfc0d6068b353
Edward Evans <elevans2@wisc.edu>
2025-03-31 16:51:19 -0500

    Add Parameter name space and Omega compute

---
+++ src/parameter/mod.rs
@@ -0,0 +1,3 @@
+pub mod parameter;
+
+pub use parameter::omega;
---
+++ src/parameter/omega.rs
@@ -0,0 +1,19 @@
+use std::f64::consts;
+
+/// Compute the angular frequency (Omega) value.
+///
+/// This function computes the angular frequency, Omega,
+/// using the following equation:
+///
+/// ω = 2π/T
+///
+/// # Arguments
+///
+/// * `period` - The period in seconds.
+///
+/// # Returns
+///
+/// The Omega value.
+pub fn omega(period: &f64) -> f64 {
+    (2 * consts::PI)/(period)
+}
