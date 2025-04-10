2034201b306ef6396ed2b457aa0deead706feb33
Edward Evans <elevans2@wisc.edu>
2025-04-10 14:22:05 -0500

    Apply Rust lang formatter

--- src/java/java_bindings.rs
+++ src/java/java_bindings.rs
@@ -1,6 +1,6 @@
-use jni::JNIEnv;
 use jni::objects::{JClass, JDoubleArray, ReleaseMode};
-use jni::sys::{jdouble};
+use jni::sys::jdouble;
+use jni::JNIEnv;
 
 use crate::statistics::sum;
 
@@ -9,12 +9,10 @@ use crate::statistics::sum;
 pub extern "system" fn Java_org_imgal_statistics_NativeSum_nativeSum(
     mut env: JNIEnv,
     _class: JClass,
-    array: JDoubleArray
-    ) -> jdouble {
+    array: JDoubleArray,
+) -> jdouble {
     // get unsafe array elements
-    let elements = unsafe {
-        env.get_array_elements(&array, ReleaseMode::NoCopyBack)
-    }.unwrap();
+    let elements = unsafe { env.get_array_elements(&array, ReleaseMode::NoCopyBack) }.unwrap();
 
     // compute array sum
     sum(&elements)
