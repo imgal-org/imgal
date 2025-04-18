26d5027769f02c62617bb613e3c7eaf15c9cb954
Edward Evans <elevans2@wisc.edu>
2025-04-18 15:18:30 -0500

    Add time domain phasor algorithms

--- src/phasor/time_domain.rs
+++ src/phasor/time_domain.rs
@@ -1,68 +1,85 @@
+use std::f64;
+
+use crate::integrate::midpoint;
 use crate::parameters;
 
-// TODO imaginary compute
-
-/// Compute the real or G component of lifetime data.
+/// Compute the imaginary S component of lifetime data.
 ///
-/// The real, G, component time domain equation is calculated
-/// using the following transformation using the trapezodial rule:
+/// The imaginary component, S, time domain equation is calculated
+/// using:
 ///
-/// G = ∫(data * cos(nωt) * dt) / ∫(data * dt)
+/// S = ∫(I(t) * sin(nωt) * dt) / ∫(I(t) * dt)
 ///
 /// Where 'n' and 'ω' are harmonic and omega values respectively.
 ///
 /// # Arguments
 ///
+/// * `i_data` - I(t), the decay data slice.
+/// * `period` - The period in seconds.
+/// * `harmonic` - The harmonic value, default = 1.0.
+/// * `omega` - The angular frequency, default = computed from the period.
+///
 /// # Returns
-pub fn real(
-    input: &[f64],
+///
+/// * `f64` - The imaginary, S, component.
+pub fn imaginary(
+    i_data: &[f64],
     period: f64,
     harmonic: Option<f64>,
     omega: Option<f64>,
-    integration_time: Option<&[f64]>,
 ) -> f64{
-    // compute and set optional paramters if needed
-    // Should these two be refs?
+    // set optional parameters if needed
     let h: f64 = harmonic.unwrap_or(1.0);
-    let w: f64 = omega.unwrap_or_else(|| parameters::omega(&period));
-    ///////////////
-    let use_buffer: bool;
-    let extern_int_time: &[f64];
-    let local_int_time: Vec<f64>;
+    let w: f64 = omega.unwrap_or_else(|| parameters::omega(period));
 
-    // set if using a buffer (i.e. a ref to int time)
-    match integration_time {
-        Some(time_array) => {
-            extern_int_time = time_array;
-            use_buffer = true;
-        },
-        None => {
-            local_int_time = calculate_integration_time(input.len(), &period);
-            use_buffer = false;
-        }
+    // integrate sine transform (imaginary)
+    let n: usize = i_data.len();
+    let mut buf = vec![0.0; n];
+    for i in 0..n {
+        buf[i] = i_data[i] * f64::sin(h * w * period * i as f64);
     }
-    ///////////////
-    // unwrap to ref slice. check if none.
-    // if none calculate the buffer ourselves to a new var
-    // use a match!
-    // compute the "real" integral
-    let mut sum_data: f64 = 0.0;
-    let mut g: f64 = 0.0;
-    for (i, &value) in input.enumerate() {
-        sum_data += value;
-        g += value * f64::cos(h * w * t[i]);
-    }
-    g /= sum_data
+    let i_sin_integral: f64 = midpoint(&buf, Some(period));
+    let i_integral: f64 = midpoint(&i_data, Some(period));
+    i_sin_integral / i_integral
 }
 
-fn calculate_integration_time(
-    bins: usize,
-    period: &f64,
-) -> Vec<f64> {
-    let mut time = vec![0.0; bins];
-    let dt: f64 = period / bins as f64;
-    for i in 0..bins {
-        time[i] = i as f64 * dt;
+
+/// Compute the real G component of lifetime data.
+///
+/// The real component, G, time domain equation is calculated
+/// using:
+///
+/// G = ∫(I(t) * cos(nωt) * dt) / ∫(I(t) * dt)
+///
+/// Where 'n' and 'ω' are harmonic and omega values respectively.
+///
+/// # Arguments
+///
+/// * `i_data` - I(t), the decay data slice.
+/// * `period` - The period in seconds.
+/// * `harmonic` - The harmonic value, default = 1.0.
+/// * `omega` - The angular frequency, default = computed from the period.
+///
+/// # Returns
+///
+/// * `f64` - The real, G, component.
+pub fn real(
+    i_data: &[f64],
+    period: f64,
+    harmonic: Option<f64>,
+    omega: Option<f64>,
+) -> f64 {
+    // set optional parameters if needed
+    let h: f64 = harmonic.unwrap_or(1.0);
+    let w: f64 = omega.unwrap_or_else(|| parameters::omega(period));
+
+    // integrate cosine transform (real)
+    let n: usize = i_data.len();
+    let mut buf: Vec<f64> = vec![0.0; n];
+    for i in 0..n {
+        buf[i] = i_data[i] * f64::cos(h * w * period * i as f64);
     }
-    time
+    let i_cos_integral: f64 = midpoint(&buf, Some(period));
+    let i_integral: f64 = midpoint(&i_data, Some(period));
+    i_cos_integral / i_integral
 }
