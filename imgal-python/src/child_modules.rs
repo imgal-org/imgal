7067b3be1d03d11ef4292a148b5c641d89b088d1
Edward Evans <elevans2@wisc.edu>
2025-05-05 21:26:42 -0500

    Add py_import_module function
    This commit adds the py_import_module function that adds a given module
    to Python's sys.modules dict. This commit also applies cargo fmt.

--- imgal-python/src/child_modules.rs
+++ imgal-python/src/child_modules.rs
@@ -1,3 +1,5 @@
+use std::ffi::CString;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
@@ -6,10 +8,21 @@ use super::functions;
 /// Python binding for the "integrate" submodule.
 pub fn register_integrate_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let integrate_module = PyModule::new(parent_module.py(), "integrate")?;
+    // add module to python's sys.modules
+    py_import_module("integrate");
     // add integrate submodule functions
-    integrate_module.add_function(wrap_pyfunction!(functions::py_fn_integrate_composite_simpson, &integrate_module)?)?;
-    integrate_module.add_function(wrap_pyfunction!(functions::py_fn_integrate_midpoint, &integrate_module)?)?;
-    integrate_module.add_function(wrap_pyfunction!(functions::py_fn_integrate_simpson, &integrate_module)?)?;
+    integrate_module.add_function(wrap_pyfunction!(
+        functions::py_fn_integrate_composite_simpson,
+        &integrate_module
+    )?)?;
+    integrate_module.add_function(wrap_pyfunction!(
+        functions::py_fn_integrate_midpoint,
+        &integrate_module
+    )?)?;
+    integrate_module.add_function(wrap_pyfunction!(
+        functions::py_fn_integrate_simpson,
+        &integrate_module
+    )?)?;
     // attach to parent module
     parent_module.add_submodule(&integrate_module)
 }
@@ -17,8 +30,13 @@ pub fn register_integrate_module(parent_module: &Bound<'_, PyModule>) -> PyResul
 /// Python binding for the "parameters" submodule.
 pub fn register_parameters_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let parameters_module = PyModule::new(parent_module.py(), "parameters")?;
+    // add module to python's sys.modules
+    py_import_module("parameters");
     // add parameters submodule functions
-    parameters_module.add_function(wrap_pyfunction!(functions::py_fn_parameters_omega, &parameters_module)?)?;
+    parameters_module.add_function(wrap_pyfunction!(
+        functions::py_fn_parameters_omega,
+        &parameters_module
+    )?)?;
     // attach to parent module
     parent_module.add_submodule(&parameters_module)
 }
@@ -26,8 +44,12 @@ pub fn register_parameters_module(parent_module: &Bound<'_, PyModule>) -> PyResu
 /// Python binding for the "phasor" submodule.
 pub fn register_phasor_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let phasor_module = PyModule::new(parent_module.py(), "phasor")?;
-    let time_domain_module = PyModule::new(parent_module.py(), "time_domain")?;
     let plot_module = PyModule::new(parent_module.py(), "plot")?;
+    let time_domain_module = PyModule::new(parent_module.py(), "time_domain")?;
+    // add module to python's sys.modules
+    py_import_module("phasor");
+    py_import_module("phasor.plot");
+    py_import_module("phasor.time_domain");
     // add phasor::time_domain submodule functions
     time_domain_module.add_function(wrap_pyfunction!(
         functions::py_fn_phasor_time_domain_imaginary,
@@ -55,8 +77,35 @@ pub fn register_phasor_module(parent_module: &Bound<'_, PyModule>) -> PyResult<(
 /// Python binding for the "statistics" submodule.
 pub fn register_statistics_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let statistics_module = PyModule::new(parent_module.py(), "statistics")?;
+    // add module to python's sys.modules
+    py_import_module("statistics");
     // add statistics submodule functions
-    statistics_module.add_function(wrap_pyfunction!(functions::py_fn_statistics_sum, &statistics_module)?)?;
+    statistics_module.add_function(wrap_pyfunction!(
+        functions::py_fn_statistics_sum,
+        &statistics_module
+    )?)?;
     // attach to parent module
     parent_module.add_submodule(&statistics_module)
+}
+
+/// Add a child module to Python's sys.modules dict.
+///
+/// # Description
+///
+/// This function manually adds a given module to Python's sys.modules
+/// dict. This enables imports like `import imgal_python.parameters as params`.
+///
+/// # Arguments
+///
+/// * `module_name` - The name of the module to add to sys.modules.
+fn py_import_module(module_name: &str) {
+    let import_cmd = format!(
+        "import sys; sys.modules['imgal_python.{}'] = '{}'",
+        module_name, module_name
+    );
+    let c_str_cmd =
+        CString::new(import_cmd).expect("Failed to create 'CString' module import command.");
+    Python::with_gil(|py| {
+        py.run(c_str_cmd.as_c_str(), None, None).unwrap();
+    });
 }
