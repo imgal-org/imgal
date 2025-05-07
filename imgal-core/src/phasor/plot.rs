4f095f26865c885f45ab9ca82a3971f9449b6583
Edward Evans <elevans2@wisc.edu>
2025-05-07 09:45:04 -0500

    Add single-component theta/modulation functions
    Also improve all phasor::plot function docs.

--- imgal-core/src/phasor/plot.rs
+++ imgal-core/src/phasor/plot.rs
@@ -1,8 +1,11 @@
 use std::f64;
 
-/// Compute the modulation of a phasor point.
+/// Compute the modulation of a multi-component phasor coordinate.
 ///
 /// # Description
+///
+/// The modulation of a multi-component phasor coordinate is calculated
+/// using:
 ///
 /// M = √(G² + S²)
 ///
@@ -14,17 +17,24 @@ use std::f64;
 /// # Returns
 ///
 /// * `f64` - The modulation of the G and S phasor coordinates.
-pub fn modulation(g: f64, s: f64) -> f64 {
+///
+/// # Reference
+///
+/// https://doi.org/10.1146/annurev-biophys-062920-063631
+pub fn multi_component_modulation(g: f64, s: f64) -> f64 {
     let g_sqr: f64 = g * g;
     let s_sqr: f64 = s * s;
     f64::sqrt(g_sqr + s_sqr)
 }
 
-/// Compute the theta of a phsor point.
+/// Compute the theta of a multi-component phasor coordinate.
 ///
 /// # Description
 ///
-/// theta = tan^-1 * (S / G)
+/// The theta of a multi-component phasor coordinate is calculated
+/// using:
+///
+/// θ = tan⁻¹(S / G)
 ///
 /// Computes atan(S / G) in all four quadrants using atan2.
 ///
@@ -36,6 +46,59 @@ pub fn modulation(g: f64, s: f64) -> f64 {
 /// # Returns
 ///
 /// * `f64` - The theta angle of the G and S phasor coordinates.
-pub fn theta(g: f64, s: f64) -> f64 {
+///
+/// # Reference
+///
+/// https://doi.org/10.1146/annurev-biophys-062920-063631
+pub fn multi_component_theta(g: f64, s: f64) -> f64 {
     s.atan2(g)
+}
+
+/// Compute the modulation of a single-component phasor coordinate.
+///
+/// # Description
+///
+/// The modulation of a single-component phasor coordinate is calculated
+/// using:
+///
+/// M = cos(θ)
+///
+/// # Arguments
+///
+/// * `theta` - The theta, θ, angle of the phasor coordinate.
+///
+/// # Returns
+///
+/// * `f64` - Modulation of a single-component phasor (g, s) coordiate.
+///
+/// # Reference
+///
+/// https://doi.org/10.1146/annurev-biophys-062920-063631
+pub fn single_component_modulation(theta: f64) -> f64 {
+    f64::cos(theta)
+}
+
+/// Compute the theta of a single-component phasor coordinate.
+///
+/// # Description
+///
+/// The theta of a single-component phasor coordinate is calculated
+/// using:
+///
+/// θ = tan⁻¹(ω * τ)
+///
+/// # Arguments
+///
+/// * `omega` - The omega, ω, angular frequency in radians.
+/// * `tau` - The tau, τ, lifetime in seconds.
+///
+/// # Returns
+///
+/// * `f64` - Theta, θ, of a single-component phasor (g, s) coordinate.
+///
+/// # Reference
+///
+/// https://doi.org/10.1146/annurev-biophys-062920-063631
+pub fn single_component_theta(omega: f64, tau: f64) -> f64 {
+    (omega * tau).atan()
 }
