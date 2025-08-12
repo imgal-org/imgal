b2843776d59bd8be4b0e68d2fcd8a14787a16e57
Edward Evans <elevans2@wisc.edu>
2025-08-12 16:34:53 -0500

    Add InvalidDimensionSize error

--- imgal_core/src/error/dimension.rs
+++ imgal_core/src/error/dimension.rs
@@ -3,17 +3,36 @@ use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum DimensionError {
-    InvalidAxis { axis: usize, dim_len: usize },
+    InvalidAxis {
+        axis_idx: usize,
+        dim_len: usize,
+    },
+    InvalidDimensionSize {
+        dim_a: usize,
+        dim_b: usize,
+        axis_idx: usize,
+    },
 }
-
+// "Dimension size {} of axis {} is out of bounds for dimension size {}."
 impl fmt::Display for DimensionError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            DimensionError::InvalidAxis { axis, dim_len } => {
+            DimensionError::InvalidAxis { axis_idx, dim_len } => {
                 write!(
                     f,
                     "Axis {} is out of bounds for dimension length {}.",
-                    axis, dim_len
+                    axis_idx, dim_len
+                )
+            }
+            DimensionError::InvalidDimensionSize {
+                dim_a,
+                dim_b,
+                axis_idx,
+            } => {
+                write!(
+                    f,
+                    "Dimension size {} of axis {} is out of bounds for dimension size {}.",
+                    dim_a, axis_idx, dim_b
                 )
             }
         }
