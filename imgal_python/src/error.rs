8f9483ce3e9825c7bd9e7a00d9a8af477782cb83
Edward Evans <elevans2@wisc.edu>
2025-08-15 22:00:45 -0500

    Make cargo fmt happy

--- imgal_python/src/error.rs
+++ imgal_python/src/error.rs
@@ -6,16 +6,17 @@ use imgal_core::error::DimensionError;
 /// Map DimensionError types to Python exceptions.
 pub fn map_dimension_error(err: DimensionError) -> PyErr {
     match err {
-        DimensionError::InvalidAxis { axis_idx, dim_len } => {
-            PyIndexError::new_err(format!(
-                "Axis {} is out of bounds for dimension length {}.",
-                axis_idx, dim_len
-            ))
-        },
-        DimensionError::InvalidDimensionSize { dim_a, dim_b, axis_idx } =>
-            PyValueError::new_err(format!(
-                "Dimension size {} of axis {} is out of bounds for dimension size {}.",
-                dim_a, dim_b, axis_idx
-            ))
+        DimensionError::InvalidAxis { axis_idx, dim_len } => PyIndexError::new_err(format!(
+            "Axis {} is out of bounds for dimension length {}.",
+            axis_idx, dim_len
+        )),
+        DimensionError::InvalidDimensionSize {
+            dim_a,
+            dim_b,
+            axis_idx,
+        } => PyValueError::new_err(format!(
+            "Dimension size {} of axis {} is out of bounds for dimension size {}.",
+            dim_a, dim_b, axis_idx
+        )),
     }
 }
--- imgal_python/src/lib.rs
+++ imgal_python/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod child_modules;
+mod error;
 pub mod functions;
 pub mod parent_module;
-mod error;
 mod utils;
