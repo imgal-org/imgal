02506ededdc3b19b5ac039b1ef74400d7c554dfe
Edward Evans <elevans2@wisc.edu>
2025-09-21 09:04:57 -0500

    Add weighted merge sort test

--- imgal_core/tests/test_statistics.rs
+++ imgal_core/tests/test_statistics.rs
@@ -16,3 +16,18 @@ fn statistics_sum() {
     assert_eq!(statistics::sum(int_arr.view()), 40);
     assert_eq!(statistics::sum(float_arr.view()), 51.86);
 }
+
+#[test]
+fn statistics_weighted_merge_sort_mut() {
+    // create data and associated weights
+    let mut d: [i32; 5] = [3, 10, 87, 22, 5];
+    let mut w: [f64; 5] = [0.51, 12.83, 4.24, 9.25, 0.32];
+
+    // sort the data and weights, get inversion count
+    let s = statistics::weighted_merge_sort_mut(&mut d, &mut w).unwrap();
+
+    // check arrays are sorted
+    assert_eq!(d, [3, 5, 10, 22, 87]);
+    assert_eq!(w, [0.51, 0.32, 12.83, 9.25, 4.24]);
+    assert_eq!(s, 47.64239999999998);
+}
