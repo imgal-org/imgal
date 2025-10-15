14c2147c10d0f025c8c67e3e2f4c29173ef0658c
Edward Evans <elevans2@wisc.edu>
2025-10-14 21:38:06 -0500

    Clean up lib.rs doc

--- imgal/src/lib.rs
+++ imgal/src/lib.rs
@@ -1,17 +1,17 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/imgal-sc/imgal/refs/heads/main/docs/assets/imgal_logo.svg"
 )]
-//! The `imgal` (**Im**a**g**e **A**lgorithm **L**ibrary) crate is a fast and
-//! open-source scientific image processing and algorithm library.This library
+//! The `imgal` (**IM**a**G**e **A**lgorithm **L**ibrary) crate is a fast and
+//! open-source scientific image processing and algorithm library. This library
 //! is directly inspired by [imagej-ops](https://github.com/imagej/imagej-ops/),
 //! [SciJava Ops](https://github.com/scijava/scijava),
-//! [ImgLib2](https://github.com/imglib/imglib2), the ImageJ2 ecosystem.
+//! [ImgLib2](https://github.com/imglib/imglib2), and the ImageJ2 ecosystem.
 //! `imgal` library aims to offer users access to fast and well documented
-//! image algorithms.
+//! scientific image processing and analysis algorithms.
 //!
 //! ## Crate Status
 //!
-//! This crate is being iterated on and is constantly evolving.
+//! This crate is still under active development on and it's API is not stable.
 pub mod colocalization;
 pub mod distribution;
 pub mod error;
