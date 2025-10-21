2ac98fe70a74afff6fb7e36bc2661c49b61416a9
Edward Evans <elevans2@wisc.edu>
2025-10-21 16:50:04 -0500

    Add statistics::max C ABI function

--- imgal_c/src/statistics_cabi.rs
+++ imgal_c/src/statistics_cabi.rs
@@ -1,8 +1,24 @@
 use std::slice;
 
+use ndarray::{ArrayViewD, IxDyn};
+
 use imgal::statistics;
 
-// C ABI inteface for imgal::statistics::sum.
+#[unsafe(no_mangle)]
+pub extern "C" fn max(ptr: *const f64, len: usize) -> f64 {
+    // validate the pointer and array length
+    if ptr.is_null() || len == 0 {
+        return 0.0;
+    }
+
+    // create slice from pointer and len, compute max
+    let data = unsafe { slice::from_raw_parts(ptr, len) };
+    let shape = IxDyn(&[data.len()]);
+    let arr = ArrayViewD::from_shape(shape, data);
+
+    statistics::max(arr.unwrap())
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn sum(ptr: *const f64, len: usize) -> f64 {
     // saftey check: validate the pointer and array length
