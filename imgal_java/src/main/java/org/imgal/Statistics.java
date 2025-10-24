5b36ffa995073ad0533046fea9a5361c58aeff90
Edward Evans <elevans2@wisc.edu>
2025-10-24 16:28:58 -0500

    Use ofArray and the critical linker option
    On heap memory access is not allowed be default for
    stability reasons. My guess is that it may interfer
    with the JVM's garbage collector. Not too sure. The
    critical linker option is intended for "short" functions
    that need memory access. I'm unsure what the consequences
    are of using the critical option, but I will test.
    
    Ideally, if this works then I don't need to copy
    data into of heap memory.

--- imgal_java/src/main/java/org/imgal/Statistics.java
+++ imgal_java/src/main/java/org/imgal/Statistics.java
@@ -2,6 +2,7 @@ package org.imgal;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodHandle;
@@ -22,21 +23,19 @@ public class Statistics extends AbstractNativeLibrary {
 	 * @return The sum of the array.
 	 */
 	public static double sum(double[] input) throws Throwable {
-		MemorySegment segment = null;
 		try (Arena arena = Arena.ofConfined()) {
 			// allocate memory for the array and obtain array length
-			segment = arena.allocateFrom(ValueLayout.JAVA_DOUBLE, input);
+			MemorySegment arr = MemorySegment.ofArray(input);
 			long len = (long) input.length;
 
-			return (double) sumMH.invokeExact(segment, len);
-		}
+			return (double) sumMH.invokeExact(arr, len); }
 	}
 
 	/**
-	 * Initialize a MethodHandle for image::statistics::sum.
+	 * Initialize a MethodHandle for imgal::statistics::sum.
 	 */
 	private static MethodHandle initSumMH() {
-		// locate the function address
+		// locate the function symbol
 		MemorySegment fnMS = libLookup.find("sum").orElseThrow();
 
 		// create a function descriptor
@@ -46,6 +45,6 @@ public class Statistics extends AbstractNativeLibrary {
 				ValueLayout.JAVA_LONG);
 
 		// return the downcall handle
-		return linker.downcallHandle(fnMS, fnSig);
+		return linker.downcallHandle(fnMS, fnSig, Linker.Option.critical(true));
 	}
 }
